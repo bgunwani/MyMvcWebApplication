85ac05aa36aa3c5d40fe43fdf866622bee25df15
anshuldhuryani <anshul.dhuryani@gmail.com>
2024-12-09 15:08:16 +0530

    Product Model and Controller Added.

---
+++ MyMvcWebApplication/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyMvcWebApplication.Models;
+
+namespace MyMvcWebApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Product> Products =
+        [
+            new Product() { Id = 101, Name = "Laptop", Price = 1200 },
+            new Product() { Id = 102, Name = "Mouse", Price = 3200 }
+        ];
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            return View(Products);
+        }
+    }
+}
